5bb495e059b66fcba31a34f89a7b0ffa5b8566bb
MLDODSON <mldodson@fullsail.edu>
2018-04-30 14:47:59 -0500

    php practice

---
+++ phpstuff/01_FirstLesson/first.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My First PHP Page</title>
+	</head>
+	<body>
+		<?php print("Time to learn something new!"); ?>
+	</body>
+</html>
---
+++ test.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>First PHP</title>
+</head>
+
+<body>
+	<?php print("My first php!"); ?>
+</body>
+</html>
