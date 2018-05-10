391968c2223987f9f9163a732a0a76e6be74ee56
MLDODSON <mldodson@fullsail.edu>
2018-05-10 13:41:08 -0500

    final project pt.1

---
+++ phpstuff/finalproject/index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+	
+<body id="final-example">
+	
+	<div class="wrapper">
+		
+		<div id="banner">
+			<a href="/" title="Return To Home">
+				<img src="img/banner.png" alt="Franklin's Fine Dining">
+			</a>
+			
+		</div><!-- banner -->
+		
+		<div id="nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="team.php">Team</a></li>
+				<li><a href="menu.php">Menu</a></li>
+				<li><a href="contact.php">Contact</a></li>
+			</ul>
+			
+		</div><!-- nav -->
+		
+		<div class="content">
+			
+			<div id="footer" class="cf">
+				
+				<div class="column three">
+					<strong>PHONE</strong>
+					888.369.8278
+				</div><!-- column -->
+				
+				
+				<div class="column three">
+					<strong>LOCATION</strong>
+					3958 Funklords Boulevard<br>
+					St. Louis, MO
+				</div><!-- column -->
+				
+				
+				<div class="column three last">
+					
+					<strong>HOURS</strong>
+					<em>Monday - Friday</em><br>
+					9:00am - 4:00pm<br><br>
+					
+					<em>Saturday</em><br>
+					9:00am - 12:00pm<br><br>
+					
+					<em>Sunday</em><br>
+					Closed<br><br>
+					
+				</div><!-- column -->
+				
+			</div><!-- footer -->
+			
+			<small>&copy;2018 Mad Mike</small>
+			
+		</div><!-- content -->
+		
+	</div><!-- wrapper -->
+	
+	<div class="copyright-info">
+		<?php include("../includes/copyright.php") ?>
+	</div><!-- copyright-info -->
+		
+</body>
+</html>
