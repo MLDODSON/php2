40dfa9fd41a6d071a9dc70ffa014c47bb2778da5
MLDODSON <mldodson@fullsail.edu>
2018-05-12 11:44:26 -0500

    final project, pt. 5, Menu and pt.6, Menu Items

--- phpstuff/finalproject/includes/arrays.php
+++ phpstuff/finalproject/includes/arrays.php
@@ -58,6 +58,48 @@
 			),
 	
 	);
+
+// Menu Items
+
+	$menuItems = array(
+		
+		"club-sandwich" => array(
+			
+			title    => "Club Sandwich",
+			price    => 11,
+			blurb    => "Frankie is the great-grandson of the original Franklin. He is the owner of Franklin's Fine Dining. He cooks a   			  mean fritatta!",
+		    drink 	 => "Club Soda"
+		
+			),
+	
+		"dill-salmon" => array(
+			
+			title    => "Lemon &amp; Dill Salmon",
+			price    => 18,
+			blurb    => "Pork belly tempor ground round qui exercitation, jowl leberkas sed voluptate excepteur jerky. Reprehenderit 	              veniam cow, quis in ribeye andouille eu pastrami eiusmod exercitation dolor.",
+		    drink 	 => "Fancy Wine"
+		
+			),
+		
+		"super-salad" => array(
+			
+			title    => "The Super Salad<sup>&reg;</sup>",
+			price    => 34,
+			blurb    => "Gumbo beet greens corn soko endive gumbo gourd. Parsley shallot courgette tatsoi pea sprouts fava bean collard 	         greens dandelion okra wakame tomato. Dandelion cucumber earthnut pea peanut soko zucchini.",
+		    drink 	 => "Jug o' Water"
+		
+			),
+		
+		"mexican-barbacoa" => array(
+			
+			title    => "Mexican Barbacoa",
+			price    => 23,
+			blurb    => "I love fruitcake danish caramels. Tart danish pastry liquorice chocolate cake fruitcake. Bear claw gingerbread 	         muffin I love apple pie apple pie tiramisu brownie chocolate. Sweet roll cotton candy cupcake gingerbread 	      	           gummies jelly-o. Muffin I love croissant I love jelly-o brownie jelly beans. Toffee I love pastry.",
+		    drink 	 => "Beer with a lime"
+		
+			),
+	
+	);
 	
 	
 ?>
--- phpstuff/finalproject/includes/header.php
+++ phpstuff/finalproject/includes/header.php
@@ -8,6 +8,7 @@
 <html>
 <head>
 	<title><?php echo TITLE; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
 	
